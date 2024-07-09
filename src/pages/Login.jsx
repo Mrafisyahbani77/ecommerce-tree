5bc73797a2f3ad2bd6a5bddff23ca6f5aac33359
Muhammad Hafidz <143271950+HapidzGIT@users.noreply.github.com>
2024-07-09 17:13:47 +0700

    Login done styling

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,11 +1,11 @@
-import Navbar from "../component/Navbar";
-import { useForm } from "react-hook-form";
-import * as yup from "yup";
-import { yupResolver } from "@hookform/resolvers/yup";
-import { useMutationLogin } from "../useMutation/useMutationLogin";
-import React from "react";
-import { useMutation } from "@tanstack/react-query";
-import { AxiosInstance } from "../AxiosInstance";
+import Navbar from '../component/Navbar';
+import { useForm } from 'react-hook-form';
+import * as yup from 'yup';
+import { yupResolver } from '@hookform/resolvers/yup';
+import { useMutationLogin } from '../useMutation/useMutationLogin';
+import React from 'react';
+import { useMutation } from '@tanstack/react-query';
+import { AxiosInstance } from '../AxiosInstance';
 
 const Login = () => {
   const schema = yup.object().shape({
@@ -13,8 +13,8 @@ const Login = () => {
     password: yup
       .string()
       .required()
-      .min(4, "Password harus memiliki 4 huruf - minimum harus ada 4 chars.")
-      .matches(/^.{4,}$/, "Minimal Memiliki 4 karakter"),
+      .min(4, 'Password harus memiliki 4 huruf - minimum harus ada 4 chars.')
+      .matches(/^.{4,}$/, 'Minimal Memiliki 4 karakter'),
   });
 
   const {
@@ -42,40 +42,25 @@ const Login = () => {
       <div className="flex items-center justify-center min-h-screen">
         <div className="w-full max-w-md p-8 space-y-8 bg-white rounded-lg shadow">
           <div className="text-center">
-            <span className="text-2xl font-bold text-gray-900">
-              E-Commerce-Tree
-            </span>
+            <span className="text-2xl font-bold text-gray-900">E-Commerce-Tree</span>
           </div>
           <form onSubmit={handleSubmit(submited)} className="space-y-6">
             <div>
-              <label
-                htmlFor="email"
-                className="block text-sm font-medium text-gray-700"
-              >
+              <label htmlFor="email" className="block text-sm font-medium text-gray-700">
                 Email
               </label>
-              <input
-                type="email"
-                name="email"
-                id="email"
-                {...register("email")}
-                className="block w-full px-3 py-2 mt-1 border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-                required
-              />
+              <input type="email" name="email" id="email" {...register('email')} className="block w-full px-3 py-2 mt-1 border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" required />
               <p className="text-red-600">{errors.email?.message}</p>
             </div>
             <div>
-              <label
-                htmlFor="password"
-                className="block text-sm font-medium text-gray-700"
-              >
+              <label htmlFor="password" className="block text-sm font-medium text-gray-700">
                 Password
               </label>
               <input
                 type="password"
                 name="password"
                 id="password"
-                {...register("password")}
+                {...register('password')}
                 className="block w-full px-3 py-2 mt-1 border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
                 required
               />
