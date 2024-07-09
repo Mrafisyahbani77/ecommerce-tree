656121c6faafa0ab215000cbb4d4d52d1a102265
Muhammad Hafidz <143271950+HapidzGIT@users.noreply.github.com>
2024-07-09 22:25:38 +0700

    DONE LOGIC LOGIN

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,13 +1,72 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import Navbar from '../component/Navbar';
+import { useForm } from 'react-hook-form';
+import * as yup from 'yup';
+import { yupResolver } from '@hookform/resolvers/yup';
+import { useMutationRegister } from '../useMutation/useMutationRegister';
 
 const Register = () => {
+  const schema = yup.object().shape({
+    username: yup.string().required('Username Wajib Diisi'),
+    email: yup.string().email().required('Email Wajib Diisi'),
+    no_telp: yup.string().min(8).required('No Telepon Wajib Diisi'),
+    password: yup
+      .string()
+      .required('Password Wajib Diisi')
+      .min(8, 'Password harus memiliki 8 huruf - minimum harus ada 8 chars.')
+      .matches(/^.{8,}$/, 'Minimal Memiliki 8 karakter'),
+  });
+
+  const Submitted = (data) => {
+    mutate(data);
+  };
+  const {
+    handleSubmit,
+    register,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schema),
+  });
+ 
+  const {mutate, } = useMutationRegister()
+
   return (
     <>
       <Navbar />
-      <div className="text-center flex justify-center underline">
-        <Link to="/login">Register</Link>
+      <div className="max-w-md p-8 mx-auto bg-slate-200 rounded-md my-10">
+        <h1 className="text-center mb-5 text-3xl font-bold  bg-gradient-to-r bg-clip-text text-transparent from-indigo-500 via-indigo-400 to-cyan-500">Register Here</h1>
+        <form className="space-y-5" onSubmit={handleSubmit(Submitted)}>
+          <div>
+            <label className="text-xl font-semibold text-slate-800">Username</label>
+            <input type="text" name="username" id="username" className="block w-full rounded-md py-2 px-3 mb-3 mt-2" {...register('username')} />
+            <p className="text-red-600">{errors.username?.message}</p>
+          </div>
+          <div>
+            <label className="text-xl font-semibold text-slate-800">Email</label>
+            <input type="email" name="email" id="" className="block  w-full rounded-md py-2 px-3 mb-3 mt-2" {...register('email')} />
+            <p className="text-red-600">{errors.email?.message}</p>
+          </div>
+          <div>
+            <label className="text-xl font-semibold text-slate-800">No Telepon</label>
+            <input type="number" name="no_telp" id="no_telp" className="block w-full rounded-md py-2 px-3 mb-3 mt-2" {...register('no_telp')} />
+            <p className="text-red-600">{errors.no_telp?.message}</p>
+          </div>
+          <div>
+            <label className="text-xl font-semibold text-slate-800">Password</label>
+            <input type="password" name="password" id="" className="block w-full rounded-md py-2 px-3 mb-3 mt-2" {...register('password')} />
+            <p className="text-red-600">{errors.password?.message}</p>
+          </div>
+          <button type="submit" className="w-full font-semibold bg-indigo-500 text-xl py-3 rounded-md text-white hover:bg-indigo-700 transition duration-300 ease-in-out shadow-xl shadow-indigo-300">
+            Submit
+          </button>
+          <div className="w-full px-3 text-lg">
+            {' '}
+            <Link to="/login">
+              Sudah Memiliki Akun? Login <span className="underline text-sky-500">Disini</span>
+            </Link>
+          </div>
+        </form>
       </div>
     </>
   );
--- src/useMutation/useMutationLogin.js
+++ src/useMutation/useMutationLogin.js
@@ -2,15 +2,37 @@ import { useMutation } from '@tanstack/react-query';
 import toast from 'react-hot-toast';
 import { AxiosInstance } from '../AxiosInstance';
 import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
 
 export const useMutationLogin = () => {
   const [onSubmit, setOnSubmit] = useState(false);
+  const navigate = useNavigate()
   const { mutate } = useMutation({
     mutationKey: ['login'],
     mutationFn: async (data) => {
       setOnSubmit(true);
       const response = await AxiosInstance.post('/login', data);
-      console.log(response);
+      const {
+        success,
+        role,
+        token,
+        permissions,
+        user: { email, username },
+      } = response.data;
+      console.log(response.data);
+      if (success) {
+     localStorage.setItem('username', username)
+     localStorage.setItem('email', email)
+     localStorage.setItem('token', token)
+     localStorage.setItem('permission', permissions)
+     localStorage.setItem('role', role)
+      }
+ if(role.includes('admin')) {
+        navigate('/adminDashboard');
+      } else {
+        navigate('userDashboard');
+      }
       setOnSubmit(false);
       return response;
     },
@@ -19,14 +41,15 @@ export const useMutationLogin = () => {
         position: 'top-right',
         duration: 3000,
       });
+    
     },
 
-    onError : () => {
-      toast.error('Password atau Email anda salah',{
+    onError: () => {
+      toast.error('Password atau Email anda salah', {
         position: 'top-right',
         duration: 3000,
-      })
-    }
+      });
+    },
   });
-  return { mutate, onSubmit, };
+  return { mutate, onSubmit };
 };
