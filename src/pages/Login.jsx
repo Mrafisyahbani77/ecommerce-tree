feb9f6bf595408b2993e9d60a54c31e5dcdb4207
Mrafisyahbani77 <mrafi.syahbani@gmail.com>
2024-07-21 10:59:45 +0700

    login,register

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,10 +1,13 @@
-import Navbar from '../component/Navbar';
-import { useForm } from 'react-hook-form';
-import * as yup from 'yup';
-import { yupResolver } from '@hookform/resolvers/yup';
-import { useMutationLogin } from '../useMutation/useMutationLogin';
-import React from 'react';
-import { Link } from 'react-router-dom';
+import Navbar from "../component/Navbar";
+import { useForm } from "react-hook-form";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { useMutationLogin } from "../useMutation/useMutationLogin";
+import React from "react";
+import { Link } from "react-router-dom";
+//icons
+import { MdEmail } from "react-icons/md";
+import { RiLockPasswordFill } from "react-icons/ri";
 
 const Login = () => {
   const schema = yup.object().shape({
@@ -12,8 +15,8 @@ const Login = () => {
     password: yup
       .string()
       .required()
-      .min(4, 'Password harus memiliki 4 huruf - minimum harus ada 4 chars.')
-      .matches(/^.{4,}$/, 'Minimal Memiliki 4 karakter'),
+      .min(4, "Password harus memiliki 4 huruf - minimum harus ada 4 chars.")
+      .matches(/^.{4,}$/, "Minimal Memiliki 4 karakter"),
   });
 
   const {
@@ -24,8 +27,7 @@ const Login = () => {
     resolver: yupResolver(schema),
   });
 
-  const { mutate: SubmitLogin, onSubmit,  } = useMutationLogin();
-
+  const { mutate: SubmitLogin, onSubmit } = useMutationLogin();
 
   const submited = (data) => {
     SubmitLogin(data);
@@ -38,38 +40,58 @@ const Login = () => {
       <div className="flex items-center justify-center min-h-svh">
         <div className="w-full max-w-md p-8 space-y-8 bg-white rounded-lg shadow">
           <div className="text-center">
-            <span className="text-2xl font-bold text-gray-900">E-Commerce-Tree</span>
+            <span className="text-2xl font-bold text-gray-900">
+              E-Commerce-Tree
+            </span>
           </div>
           <form onSubmit={handleSubmit(submited)} className="space-y-6">
             <div>
-              <label htmlFor="email" className="block text-sm font-medium text-gray-700">
+              <label
+                htmlFor="email"
+                className="flex text-sm font-medium text-gray-700"
+              >
                 Email
+                <MdEmail className="ml-1 h-5 w-4" />
               </label>
-              <input type="email" name="email" id="email" {...register('email')} className="block w-full px-3 py-2 mt-1 border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" required />
+              <input
+                type="email"
+                name="email"
+                id="email"
+                {...register("email")}
+                className="block w-full px-3 py-2 mt-1 border-slate-500 border-2 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                required
+              />
               <p className="text-red-600">{errors.email?.message}</p>
             </div>
             <div>
-              <label htmlFor="password" className="block text-sm font-medium text-gray-700">
-                Password
+              <label
+                htmlFor="password"
+                className="flex text-sm font-medium text-gray-700"
+              >
+                Password <RiLockPasswordFill className="ml-1 w-4 h-4" />
               </label>
               <input
                 type="password"
                 name="password"
                 id="password"
-                {...register('password')}
-                className="block w-full px-3 py-2 mt-1 border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                {...register("password")}
+                className="block w-full px-3 py-2 mt-1 border-slate-500 border-2 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
                 required
               />
               <p className="text-red-600">{errors.password?.message}</p>
             </div>
+
             <div>
               <button
                 type="submit"
                 className="w-full px-4 py-2 font-medium text-white bg-indigo-600 border border-transparent rounded-md shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
               >
-                {onSubmit ? 'Loginned...' : 'Login'}
+                {onSubmit ? "Loginned..." : "Login"}
               </button>
-              <Link to='/register' className='block pt-5 font-medium text-md'>Belum Punya Akun? Register <span className='text-cyan-400 underline'>Disini</span></Link>
+              <Link to="/register" className="block pt-5 font-medium text-md">
+                Belum Punya Akun? Register{" "}
+                <span className="text-cyan-400 underline">Disini</span>
+              </Link>
             </div>
           </form>
         </div>
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -5,6 +5,13 @@ import { useForm } from 'react-hook-form';
 import * as yup from 'yup';
 import { yupResolver } from '@hookform/resolvers/yup';
 import { useMutationRegister } from '../useMutation/useMutationRegister';
+//icons
+import { FaUser } from "react-icons/fa";
+import { MdEmail } from "react-icons/md";
+import { RiLockPasswordFill } from "react-icons/ri";
+import { MdPhoneAndroid } from "react-icons/md";
+
+
 
 const Register = () => {
   const schema = yup.object().shape({
@@ -34,30 +41,30 @@ const Register = () => {
   return (
     <>
       <Navbar />
-      <div className="max-w-md p-8 mx-auto hover:shadow-gray-700 hover:shadow-lg  bg-slate-200 rounded-md my-10">
-        <h1 className="text-center mb-5 text-3xl font-bold  bg-gradient-to-r bg-clip-text text-transparent from-indigo-500 via-indigo-400 to-cyan-500">Register Here</h1>
+      <div className="max-w-md p-8 mx-auto border-slate-500 border-2  bg-slate-200 rounded-md my-10">
+        <h1 className="text-center mb-10 text-3xl font-bold  bg-gradient-to-r bg-clip-text text-transparent from-indigo-500 via-indigo-400 to-cyan-500">Register</h1>
         <form className="space-y-5" onSubmit={handleSubmit(Submitted)}>
           <div>
-            <label className="text-xl font-semibold text-slate-800">Username</label>
-            <input type="text" name="username" id="username" className="block w-full rounded-md py-2 px-3 mb-3 mt-2 border-2 border-slate-300" {...register('username')} />
+            <label className="text-md flex font-semibold text-slate-800">Username<FaUser className='ml-1 w-3 my-1'/></label>
+            <input type="text" name="username" id="username" className="block w-full rounded-md py-2 px-3 mb-3 mt-2 border-2 border-slate-500" {...register('username')} />
             <p className="text-red-600">{errors.username?.message}</p>
           </div>
           <div>
-            <label className="text-xl font-semibold text-slate-800">Email</label>
-            <input type="email" name="email" id="email" className="block  w-full rounded-md py-2 px-3 mb-3 mt-2" {...register('email')} />
+            <label className="text-md flex font-semibold text-slate-800">Email<MdEmail className='ml-1 w-4 my-1'/></label>
+            <input type="email" name="email" id="email" className="block border-slate-500 border-2  w-full rounded-md py-2 px-3 mb-3 mt-2" {...register('email')} />
             <p className="text-red-600">{errors.email?.message}</p>
           </div>
           <div>
-            <label className="text-xl font-semibold text-slate-800">No Telepon</label>
-            <input type="number" name="no_telp" id="no_telp" className="block w-full rounded-md py-2 px-3 mb-3 mt-2" {...register('no_telp')} />
+            <label className="text-md flex font-semibold text-slate-800">No Telepon<MdPhoneAndroid className='ml-1 w-4 my-1'/></label>
+            <input type="number" name="no_telp" id="no_telp" className="block w-full border-2 border-slate-500 rounded-md py-2 px-3 mb-3 mt-2" {...register('no_telp')} />
             <p className="text-red-600">{errors.no_telp?.message}</p>
           </div>
           <div>
-            <label className="text-xl font-semibold text-slate-800">Password</label>
-            <input type="password" name="password" id="password" className="block w-full rounded-md py-2 px-3 mb-3 mt-2" {...register('password')} />
+            <label className="text-md font-semibold flex text-slate-800">Password<RiLockPasswordFill className='ml-1 w-4 my-1'/></label>
+            <input type="password" name="password" id="password" className="block border-2 border-slate-500 w-full rounded-md py-2 px-3 mb-3 mt-2" {...register('password')} />
             <p className="text-red-600">{errors.password?.message}</p>
           </div>
-          <button type="submit" className="w-full font-semibold bg-indigo-500 text-xl py-3 rounded-md text-white hover:bg-indigo-700 transition duration-300 ease-in-out shadow-xl shadow-indigo-300">
+          <button type="submit" className="w-full font-semibold bg-indigo-500  text-xl py-3 rounded-md text-white hover:bg-indigo-700 transition duration-300 ease-in-out shadow-xl shadow-indigo-300">
             Submit
           </button>
           <div className="w-full px-3 text-lg">
