bb9515f2ed9ef73fd00a047f1071a2babd059b83
Muhammad Hafidz <143271950+HapidzGIT@users.noreply.github.com>
2024-07-22 11:46:38 +0700

    DESIGN IN CARD nya

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,4 +1,3 @@
-  // import React, { useContext, useEffect, useState } from 'react';
   import { useFetch } from '../useFetchProduct/useFetch';
   import Banner from './Banner';
   import { ShoppingCart } from 'lucide-react';
@@ -9,7 +8,6 @@
 
 const Product = () => {
   const { data: products, isLoading, error } = useFetch();
-  // const { addToCart } = useContext(AppContext);
   if (isLoading) {
     return <div>Loading...</div>;
   }
@@ -31,12 +29,14 @@ const Product = () => {
       <section id="home" className="pt-10 pb-12 container px-4">
         <h1 className='text-xl font-bold py-5 md:text-2xl lg:text-3xl'>Kategori Pilihan</h1>
         <hr/>
-        <div className=" gap-10 my-8 grid grid-cols-1 lg:grid lg:grid-cols-3 md:grid md:grid-cols-2 sm:items-center">
+        <div className="md:grid grid-cols-2 lg:grid lg:grid-cols-3 max-w-full">
           {products?.map((product) => (
             <div key={product.id} className="rounded-md bg-white shadow-md p-5 mx-auto">
-              <img src={product.image_url} alt={product.name} className="object-cover rounded-md  max-w-full overflow-hidden md:max-h-52 max-h-80 mx-auto" />
-              <div className="mt-10 mb-3 px-4">
-                <h1 className="py-2 text-2xl font-semibold text-slate-800 truncate">{product.name}</h1>
+              <div className='object-cover rounded-md overflow-hidden'>
+                <img src={product.image_url} alt={product.name} className='mx-auto max-w-md max-h-48' />
+              </div>
+              <div className="mt-10 mb-3 flex flex-col justify-center truncate">
+                <h1 className="py-2 text-xl font-semibold text-slate-800 truncate">{product.name}</h1>
                 <h2 className="text-xl font-medium text-red-400">IDR : {product.price}</h2>
                 <p className="py-3 text-lg font-medium">
                   Category :<span className="bg-slate-600 text-white px-3 py-2 rounded-full mx-3 text-center"> {product.merchant}</span>
